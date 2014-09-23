c9d6b25d558dfb304131a7fb66d968f34a4d4267
Ryan D'souza <dsouzarc@gmail.com>
2014-09-23 13:49:53 -0400

    Changes

--- server.js
+++ server.js
@@ -4,5 +4,4 @@ http.createServer(function(myRequest, myResponse) {
 	myResponse.writeHead(200, {"Content-Type": "text/plain"});
 	myResponse.write("Hello World! Hahaaha");
 	myResponse.end();
-});.listen(1996);
-
+}).listen(1996);
