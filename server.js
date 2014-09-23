69af9b180dfd76531bfc3169ddd0cc15c5e1fc0f
Ryan D'souza <dsouzarc@gmail.com>
2014-09-23 13:47:21 -0400

    My server

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require("http");
+
+http.createServer(function(myRequest, myResponse) { 
+	myResponse.writeHead(200, {"Content-Type": "text/plain"});
+	myResponse.write("Hello World! Hahaaha");
+	myResponse.end();
+});.listen(1996);
+
