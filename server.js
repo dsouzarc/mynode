f9e1748e6ed4eb6bcfd23644bf25ddce517655e3
Ryan D'souza <dsouzarc@gmail.com>
2014-09-24 13:10:25 -0400

    start() for http

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 var http = require("http");
 
+function start() { 
+	function onRequest(request, response) { 
+		console.log("Received request");
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.write("From start()");
+		response.end();
+	}
+
+	http.createServer(onRequest).listen(1996);
+	console.log("Server started");
+}
+
 http.createServer(function(myRequest, myResponse) { 
 	myResponse.writeHead(200, {"Content-Type": "text/plain"});
 	myResponse.write("Hello World! Hahaaha");
