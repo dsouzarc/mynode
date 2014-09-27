e89d13e5cbee5ab30d111f910aa07bb5203c7fd1
Ryan D'souza <dsouzarc@gmail.com>
2014-09-27 14:12:57 -0400

    adds handle as parameter

--- router.js
+++ router.js
@@ -1,5 +1,12 @@
-function route(pathname) { 
+function route(handle, pathname) { 
 	console.log("Routing for: " + pathname);
+
+	if(typof handle[pathname] === 'function') { 
+		handle[pathname]();
+	}
+	else {
+		console.log("No handle found for: " + pathname); 
+	}
 }
 
 exports.route = route;
